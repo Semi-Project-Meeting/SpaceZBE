099e3cb6cb17c3ddefa1ba651905f8215c434ea3
hmhmchm <hyeonminchoi5@gmail.com>
2022-10-28 11:16:54 +0900

    feature : 예약 취소기능  (오피스,데스크,회의실)

--- SpaceZBE/src/main/java/com/spaceZ/reservation/ReservationDAOimpl.java
+++ SpaceZBE/src/main/java/com/spaceZ/reservation/ReservationDAOimpl.java
@@ -17,6 +17,7 @@ public class ReservationDAOimpl implements ReservationDAO {
 	SqlSession sqlSession;
 	
 	@Override
+	// 예약하기
 	public int reserve(ReservationVO vo) {
 		int flag = sqlSession.insert("SQL_RESERVE",vo);
 		
@@ -24,11 +25,42 @@ public class ReservationDAOimpl implements ReservationDAO {
 	}
 	
 	@Override
-	public int resvCancel(ReservationVO vo) {
-		return 0;
+	// 오피스 예약 취소하기 (status 상태만 예약 취소로 변경한다.)
+	public int officeCancel(ReservationVO vo) {
+		
+		int flag = sqlSession.delete("SQL_RESERVE_CANCEL",vo);
+		
+		return flag;
+	}
+	@Override
+	// 데스크 회의실 예약 취소하기 (status 상태를 예약 취소로 바꾸고 결제만 환불 된다.)
+	public int deskCancel(String reservationId) {
+		logger.info("desk-Cancel..");
+		
+		// 예약 번호에 대한 예약한 시간 가져오기. 
+		float reverse_time = sqlSession.selectOne("SQL_RESERVE_TIME",reservationId);
+		
+		logger.info("reverse_time : {}",reverse_time);
+		
+		// 예약한지 1시간 이내일 때 (전체 환불)
+		if(reverse_time <= 1) {
+			logger.info("{} : 예약한지 1시간이 지나지 않았습니다. (전체 환불 가능)",reverse_time);
+			
+		} 
+		// 예약한지 1시간 이후일  (선결제만 환불 가능, 보증금 환불 불가능)
+		else if(reverse_time > 1) {
+			logger.info("{} : 예약한지 1시간이 지났으요 (보증금만 환불 가능)",reverse_time);
+			
+		}
+		
+		// 예약중 -> 예약취소
+		int flag = sqlSession.delete("SQL_RESERVE_CANCEL",reservationId);
+		
+		return flag;
 	}
 
 	@Override
+	// 예약 날짜 중복 확인 
 	public List<ReservationVO> resvDuplicate(ReservationVO vo) {
 		List<ReservationVO> vos = sqlSession.selectList("SQL_RESERVE_DUPLICATE",vo);
 		
