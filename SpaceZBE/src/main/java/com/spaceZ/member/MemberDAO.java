33ebcb59c26da6c1aeb86a50c04086d956100381
journeypark93 <wwwsomty@gmail.com>
2022-10-29 14:30:27 +0900

    feat : 이메일 인증, 아이디중복확인, CORS filter 추가

--- SpaceZBE/src/main/java/com/spaceZ/member/MemberDAO.java
+++ SpaceZBE/src/main/java/com/spaceZ/member/MemberDAO.java
@@ -1,10 +1,12 @@
 package com.spaceZ.member;
 
+import javax.mail.MessagingException;
+
 public interface MemberDAO {
 	
 	public int signUp(MemberVO vo);
 	public int login(MemberVO vo);
 	public int idCheck(MemberVO vo);
-	public int sendEmail(MemberVO vo);
+	public int sendEmail(EmailVO vo) throws MessagingException;
 	
 }
